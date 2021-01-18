d813d538fbdb264dfbe798823942b9809790a8a3
Nikita Sharma <nikita@cs.utexas.edu>
2021-01-17 18:18:46 -0600

    parse in faq xml and generate html output

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-var particles = NaN;
-
 window.onload = function() {
-    particles = Particles.init({
-        selector: '.background',
-        connectParticles: true,
-        color: '#999999',
-        sizeVariations: 3,
-        maxParticles: 100,
-
-        responsive: [
-            {
-                breakpoint: 1000,
-                options: {
-                    maxParticles: 0
-                }
-            }
-        ]
-    });
+    loadXMLDoc();
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -35,13 +18,65 @@ function changemode() {
     let root = document.documentElement;
     if (current) {
         root.style.setProperty('--bg', '#293438');
-        root.style.setProperty('--fg', '#ffffff');
-        root.style.setProperty('--shadow', '#cccccc');
+        root.style.setProperty('--fg', '#F1F5F3');
+        root.style.setProperty('--shadow', '#F1F5F3');
         current = false;
     } else {
         root.style.setProperty('--fg', '#293438');
-        root.style.setProperty('--bg', '#ffffff');
+        root.style.setProperty('--bg', '#F1F5F3');
         root.style.setProperty('--shadow', '#D3DED9');
         current = true;
     }
+}
+
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+
+    };
+    xmlhttp.open("GET", "faq.xml" , true);
+    xmlhttp.send();
+
+}
+
+function myFunction(xml) {
+    var x, i, xmlDoc, contents;
+    xmlDoc = xml.responseXML;
+    contents = "";
+    x = xmlDoc.getElementsByTagName("item");
+    for (i = 0; i < x.length; i++) {
+        var quest = x[i].getElementsByTagName("question")[0].childNodes[0].nodeValue;
+        var ans = x[i].getElementsByTagName("answer")[0].childNodes[0].nodeValue;
+
+        var code = String.fromCharCode('a'.charCodeAt() + i);
+
+        contents += "<div class=\"question\">" +
+            "<span id=\"" + code + "_t\">" + quest + "</span>" +
+            "<a onclick=\"expand('" + code + "')\" id=\"" +
+            code + "_l\" class=\"expand\">+</a>" +
+            "<div id=\"" + code + "\" class=\"answer\">" +
+            ans + "</div></div>";
+
+    }
+
+    document.getElementById("faq").innerHTML = contents;
+
+}
+
+function expand(param_id) {
+    var div = document.getElementById(param_id);
+    var title = document.getElementById(param_id + "_t");
+    var link = document.getElementById(param_id + "_l");
+
+    if (div.style.display === "block") {
+        div.style.display = "none";
+        link.innerHTML = "+";
+    } else {
+        div.style.display = "block";
+        link.innerHTML = "-";
+    }
+
 }
